b857b3f4f7fc97a8f283db2e92f9d60f3e77ec26
Suhel Mujawar <suhelmujawar3369@gmail.com>
2026-07-11 00:00:48 +0530

    init: Intial structure of project

---
+++ src/main/java/org/example/milk_more/MilkMoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.milk_more;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkMoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilkMoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/milk_more/MilkMoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.milk_more;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkMoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
